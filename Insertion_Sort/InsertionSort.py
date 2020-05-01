023a06c81315771216cf97d5d44c31a190bce2c4
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-01 11:57:47 +0530

    Insertion Sort implementation with Python

--- Insertion_Sort/InsertionSort.py
+++ Insertion_Sort/InsertionSort.py
@@ -1,3 +1,32 @@
 def insertionSort(Arr):
-	for i in xrange(0,len(Arr)):
-		pass
+	for i in range(1,len(Arr)):
+		key = Arr[i]
+		j = i
+		while j>=1 and Arr[j-1] > key:
+			Arr[j] = Arr[j-1]
+			j = j - 1
+		Arr[j] = key
+
+def printArray(Arr):
+	output=[]
+	output.append("[")
+	for i in range(0,len(Arr)):
+		output.append(" "+Arr[i]+" ")
+		if i<len(Arr)-1:
+			output.append(",")
+	output.append("]")
+	print(''.join(output))
+
+
+N = int(input())
+Arr = []
+
+for i in range(0,N):
+	Arr.append(input())
+
+insertionSort(Arr)
+printArray(Arr)
+
+
+
+		
